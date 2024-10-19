b59ccbf90ae035114371786da0c933381828bd16
nicoleandbilal <nicolezahouani@gmail.com>
2024-10-19 04:11:26 +0100

    reorganized shared folder

--- apps/attendee_app/lib/screens/auth/login_screen.dart
+++ apps/attendee_app/lib/screens/auth/login_screen.dart
@@ -120,7 +120,7 @@ class LoginScreenState extends State<LoginScreen> {
     return const Padding(
       padding: EdgeInsets.only(top: 8.0),
       child: Text(
-        'Welcome back',
+        'Welcome back, Guest.',
         style: TextStyle(
           fontSize: 24.0,
           fontWeight: FontWeight.normal,
